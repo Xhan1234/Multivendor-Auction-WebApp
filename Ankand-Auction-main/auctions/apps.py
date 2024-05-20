7668d96ebd025c7ebbf991d433abb87c7ac89d98
Xhanluka <hxhanluka@gmail.com>
2024-05-20 21:15:38 +0200

    Add untracked files

--- Ankand-Auction-main/auctions/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class auctionsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'auctions'
-
-    def ready(self):
-        import auctions.signals  # Import the signals module
--- Ankand-Auction-main/users/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class UserRegistrationConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'users'
-
